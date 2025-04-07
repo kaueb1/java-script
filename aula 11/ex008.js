d6ebc696fcee09311f5fd3fa21edc08b2172e756
kaueb1 <kauebvieira@gmail.com>
2025-04-07 15:24:26 -0300

    Aula 12 – Condições (Parte 2)

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 123.7
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if (vel > 60) { 
+    console.log('Você utrapassou a velocidade permitida. Multado!')
+}
+console.log(`dirija sempre usando cinto de segurança`)
---
+++ aula 12/ex011.js
@@ -0,0 +1,7 @@
+var idade = 16
+if (idade < 18) {
+    console.log('Não vota')
+} else if (idade < 18) {
+    
+    console.log ('Voto opcional')
+}
