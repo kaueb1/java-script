5ab803ea1419ab8d25dd48abc7ca932b5374742f
kaueb1 <kauebvieira@gmail.com>
2025-04-09 15:37:23 -0300

    Exercícios JavaScript (Parte 2) - Refeito

--- aula 12 ex/script.js
+++ aula 12 ex/script.js
@@ -1,14 +1,14 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+    var img = window.document.getElementByI('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        img.src = 'imagens/fotomanha.png,jpeg'
+        img.src = 'imagens/fotomanha.jpeg'
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora <= 18) {
-        img.src = 'fototarde.png'
+        img.src = 'imagens/fototarde.png'
         document.body.style.background = '#b9846f'
     } else {
         img.src = 'imagens/fotonoite.png'
