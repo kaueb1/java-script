c724aef4fac9877d544923afa95b309ad6618fc1
kaueb1 <kauebvieira@gmail.com>
2025-04-14 14:09:57 -0300

    Aula 13 – Repetições (Parte 1)

---
+++ aula 13/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1
+do {
+    console.log(`Passo ${}`)
+    c++
+} while (c <= 6)
+
+
+
+
+
+
+
+var c = 1
+while (c <= 500) {
+    console.log(`Passo ${C}`)
+    c++
+}
+
+
+
+
+
+
+console.log('Oi, tudo bem?')
+console.log('Oi, tudo bem?')
+console.log('Oi, tudo bem?')
+console.log('Oi, tudo bem?')
+console.log('Oi, tudo bem?')
+console.log('Oi, tudo bem?')
