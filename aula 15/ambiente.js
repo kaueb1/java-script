2afef68b7c1a7bd2241ec66692cc6c612b7500da
kaueb1 <kauebvieira@gmail.com>
2025-04-17 11:43:37 -0300

    Exercícios JavaScript (Parte 5)

---
+++ aula 15/ambiente.js
@@ -0,0 +1,8 @@
+console.log('Vai começar!')
+for (var c = 1;c <=5;c++) {
+    console.log(c)
+}
+console.log('Fim!')
+
+//for: já sabe qqtas vezes vai repetir
+//while: ñ se sabe ao certo quantas repetições
